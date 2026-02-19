f8ecce906c13d931aa94f3e9eb7f1fef821c1c64
shamsiddin218 <mahmudovramziddin25@gmail.com>
2026-02-19 07:25:15 +0500

    feat: add thorough logging and fix previous issues

--- src/api/client.js
+++ src/api/client.js
@@ -6,7 +6,7 @@
  */
 import axios from 'axios';
 
-const API_BASE = import.meta.env.VITE_API_BASE || '';
+const API_BASE = import.meta.env.VITE_API_BASE || 'https://my-rocket-api.duckdns.org';
 
 const client = axios.create({
   baseURL: API_BASE,
